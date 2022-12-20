15986f149f8dcf9da711fa330c90fc4d796939cd
go-roii <royfgopez80@gmail.com>
2022-12-21 02:53:26 +0800

    fix comment

--- vidtomp3.sh
+++ vidtomp3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# converts mp4, mkv, and avi to mp3
+# converts mp4, mkv, and avi to mp3 using japanese audio
 
 for i in *.{mp4,mkv,avi}; do
   ffmpeg -n -i "$i" -map 0:a:m:language:jpn? "${i%.*}.mp3"
