c1e667ca58a516549e86fe3e869e1402a4d8edf2
go-roii <royfgopez80@gmail.com>
2022-12-21 02:58:49 +0800

    use double brackets instead for

--- switch_audio.sh
+++ switch_audio.sh
@@ -7,7 +7,7 @@ pacmd set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:ana
 ACTIVE_PORT_FILE='/tmp/ACTIVE_PORT'
 ACTIVE_PORT=$(cat $ACTIVE_PORT_FILE)
 
-if [ -f "$ACTIVE_PORT_FILE" -a ${ACTIVE_PORT} == 'HEADPHONES' ]; then
+if [[ -f "$ACTIVE_PORT_FILE" && $ACTIVE_PORT == 'HEADPHONES' ]]; then
   pactl set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-lineout
   echo 'LINEOUT' > $ACTIVE_PORT_FILE
   notify-send --expire-time=2000 --icon=audio-card --app-name='System' 'Active Port' 'Lineout'
