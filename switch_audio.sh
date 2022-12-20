86028d76142d722c340b0cd92757ea3ca9d4f18f
go-roii <royfgopez80@gmail.com>
2022-12-21 02:53:53 +0800

    move ACTIVE_PORT file to /tmp

--- switch_audio.sh
+++ switch_audio.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-# add command below to .bashrc
-### establish default active port
-### echo "LINEOUT" > .ACTIVE_PORT
 
 # set default audio profile to stereo 
 # so that the headphone port becomes available
 pacmd set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
 
-ACTIVE_PORT=$(cat .ACTIVE_PORT)
+ACTIVE_PORT_FILE='/tmp/ACTIVE_PORT'
+ACTIVE_PORT=$(cat $ACTIVE_PORT_FILE)
 
-if [[ ${ACTIVE_PORT} == "LINEOUT" ]]; then
-  pactl set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-headphones
-  echo "HEADPHONES" > .ACTIVE_PORT
-  notify-send --expire-time=2000 --icon=audio-card --app-name="System" "Active Port" "Headphones"
-else
+if [ -f "$ACTIVE_PORT_FILE" -a ${ACTIVE_PORT} == 'HEADPHONES' ]; then
   pactl set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-lineout
-  echo "LINEOUT" > .ACTIVE_PORT
-  notify-send --expire-time=2000 --icon=audio-card --app-name="System" "Active Port" "Lineout"
+  echo 'LINEOUT' > $ACTIVE_PORT_FILE
+  notify-send --expire-time=2000 --icon=audio-card --app-name='System' 'Active Port' 'Lineout'
+else
+  pactl set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-headphones
+  echo 'HEADPHONES' > $ACTIVE_PORT_FILE
+  notify-send --expire-time=2000 --icon=audio-card --app-name='System' 'Active Port' 'Headphones'
 fi
